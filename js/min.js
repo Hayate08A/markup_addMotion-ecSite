66289101f31c9f35440abcd7465a08aa6e2e51db
ha08ya.a <ha08ya@gmail.com>
2021-10-23 22:05:23 +0900

    [Add] スライドショーを完成させよう

--- js/min.js
+++ js/min.js
@@ -5,9 +5,12 @@
     setTimeout(() => {
       images[currentIndex].classList.remove('current');
       currentIndex++;
+      if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
       images[currentIndex].classList.add('current');
       play();
-    }, 1000);
+    }, 5000);
   }
 
   const images = document.querySelectorAll('.hero img');
